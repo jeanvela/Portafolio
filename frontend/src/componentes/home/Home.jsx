2e3dec3962cf3b771393866b78f5e201cc99b0b9
jeanpierre <alejandrovelaarana@gmail.com>
2023-06-08 20:44:56 -0300

    home-footer-navbar

---
+++ frontend/src/componentes/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home  = () => {
+    return (
+        <main>
+            <section>
+               <h1>Hola Mundo</h1>
+            </section>
+        </main>
+    )
+}
+
+export default Home
