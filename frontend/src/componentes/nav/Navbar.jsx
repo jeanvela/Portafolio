22155dfeffcc31029c5a810a8466105d23fbac63
jeanpierre <alejandrovelaarana@gmail.com>
2023-06-11 11:19:27 -0300

    commit

--- frontend/src/componentes/nav/Navbar.jsx
+++ frontend/src/componentes/nav/Navbar.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import style from "../../styles/Navbar.module.css"
 
 const Navbar = () => {
     return (
-        <header>
-            <nav></nav>
+        <header className={style.header}>
+            <span className={style.span}>Jean Pierre</span>
+            <nav className={style.navbar}>
+                <ul className={style.navbar_ul}>
+                    <li className={style.ul_li}>Home</li>
+                    <li className={style.ul_li}>About</li>
+                    <li className={style.ul_li}>Skill</li>
+                    <li className={style.ul_li}>Proyects</li>
+                    <li className={style.ul_li}>Contact</li>
+                </ul>
+            </nav>
         </header>
     )
 }
