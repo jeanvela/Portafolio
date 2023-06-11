c131e01e5393a7b1f47c798f90d607bc8ccd9ae1
jeanpierre <alejandrovelaarana@gmail.com>
2023-06-11 18:44:24 -0300

    logos

--- frontend/src/componentes/skill/Skill.jsx
+++ frontend/src/componentes/skill/Skill.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import ReactJs from "../../icons/React"
+import JavaScript from "../../icons/JavaScript";
+import Css from "../../icons/Css";
+import Express from "../../icons/Express";
+import Html from "../../icons/Html";
+import NodeJs from "../../icons/NodeJs";
+import MongoDb from "../../icons/MongoDb";
+import Mongoose from "../../icons/Mongoose";
+import Redux from "../../icons/Redux";
+import TailwindCss from "../../icons/TailwindCss";
+import PostgreSql from "../../icons/PostgreSql";
 
 const Skill = () => {
     return (
         <section>
-
+            <div>
+                <h2>SKILL</h2>
+                <div>
+                    <ReactJs/>
+                    <JavaScript/>
+                    <Html/>
+                    <Css/>
+                    <Redux/>
+                    <TailwindCss/>
+                    <NodeJs/>
+                    <Express/>
+                    <MongoDb/>
+                    <Mongoose/>
+                    <PostgreSql/>
+                </div>
+            </div>
         </section>
     )
 }
